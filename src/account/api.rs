ad2c46328f4720c142891673d91ad5665e51c1df
Ankur Srivastava <best.ankur@gmail.com>
2020-10-15 23:07:25 +0200

    more end points
    Signed-off-by: Ankur Srivastava <best.ankur@gmail.com>

--- src/account/api.rs
+++ src/account/api.rs
@@ -112,10 +112,153 @@ impl AccountApi {
 		let response = make_request!(self, reqwest::Method::GET, url)?;
 		Ok(response.json().await?)
 	}
+
+	/// Delete authentication method
+	///
+	/// https://api.aiven.io/doc/#operation/AccountAuthenticationMethodDelete
+	///
+	/// # Examples
+	/// Basic usage:
+	///
+	/// ```rust,no_run
+	/// use serde_json::json;
+	///
+	/// # #[tokio::main]
+	/// # async fn main()-> Result<(), Box<dyn std::error::Error>>{
+	///
+	/// let client = aiven_rs::AivenClient::from_token("https://api.aiven.io", "v1", "aiven-token");
+	///
+	/// // check rest of the json body from the API doc above
+	/// let response = client
+	///         .account()
+	///         .delete_auth_method("my-account-id", "account-method-id").await?;
+	/// # Ok(())
+	/// # }
+	/// ```
+	pub async fn delete_auth_method(
+		&self,
+		account_id: &str,
+		account_auth_method_id: &str,
+	) -> Result<(), AivenError> {
+		let url = &format!(
+			"account/{account_id}/authentication/{account_auth_method_id}",
+			account_id = encode_param(account_id),
+			account_auth_method_id = encode_param(account_auth_method_id),
+		);
+		let _response = make_request!(self, reqwest::Method::DELETE, url)?;
+		Ok(())
+	}
+
+	/// Get details of a single authentication method
+	///
+	/// https://api.aiven.io/doc/#operation/AccountAuthenticationMethodGet
+	///
+	/// # Examples
+	/// Basic usage:
+	///
+	/// ```rust,no_run
+	/// use serde_json::json;
+	///
+	/// # #[tokio::main]
+	/// # async fn main()-> Result<(), Box<dyn std::error::Error>>{
+	///
+	/// let client = aiven_rs::AivenClient::from_token("https://api.aiven.io", "v1", "aiven-token");
+	///
+	/// // check rest of the json body from the API doc above
+	/// let response = client
+	///         .account()
+	///         .get_auth_method_details("my-account-id", "account-method-id").await?;
+	/// # Ok(())
+	/// # }
+	/// ```
+	pub async fn get_auth_method_details(
+		&self,
+		account_id: &str,
+		account_auth_method_id: &str,
+	) -> Result<types::AuthenticationMethodResponse, AivenError> {
+		let url = &format!(
+			"account/{account_id}/authentication/{account_auth_method_id}",
+			account_id = encode_param(account_id),
+			account_auth_method_id = encode_param(account_auth_method_id),
+		);
+		let response = make_request!(self, reqwest::Method::GET, url)?;
+		Ok(response.json().await?)
+	}
+
+
+	/// Update authentication method
+	///
+	/// https://api.aiven.io/doc/#operation/AccountAuthenticationMethodUpdate
+	///
+	/// # Examples
+	/// Basic usage:
+	///
+	/// ```rust,no_run
+	/// use serde_json::json;
+	///
+	/// # #[tokio::main]
+	/// # async fn main()-> Result<(), Box<dyn std::error::Error>>{
+	///
+	/// let client = aiven_rs::AivenClient::from_token("https://api.aiven.io", "v1", "aiven-token");
+	///
+	/// // check rest of the json body from the API doc above
+	/// let response = client
+	///         .account()
+	///         .update_auth_method("my-account-id", "account-method-id").await?;
+	/// # Ok(())
+	/// # }
+	/// ```
+	pub async fn update_auth_method(
+		&self,
+		account_id: &str,
+		account_auth_method_id: &str,
+	) -> Result<types::AuthenticationMethodResponse, AivenError> {
+		let url = &format!(
+			"account/{account_id}/authentication/{account_auth_method_id}",
+			account_id = encode_param(account_id),
+			account_auth_method_id = encode_param(account_auth_method_id),
+		);
+		let response = make_request!(self, reqwest::Method::PUT, url)?;
+		Ok(response.json().await?)
+	}
+	
+
+	/// Create a new account
+	///
+	/// https://api.aiven.io/doc/#operation/AccountCreate
+	///
+	/// # Examples
+	/// Basic usage:
+	///
+	/// ```rust,no_run
+	/// use serde_json::json;
+	///
+	/// # #[tokio::main]
+	/// # async fn main()-> Result<(), Box<dyn std::error::Error>>{
+	///
+	/// let client = aiven_rs::AivenClient::from_token("https://api.aiven.io", "v1", "aiven-token");
+	///
+	/// // check rest of the json body from the API doc above
+	/// let response = client
+	///         .account()
+	///         .create_new("my-account-name").await?;
+	/// # Ok(())
+	/// # }
+	/// ```
+	pub async fn create_new(
+		&self,
+		account_name: &str,
+	) -> Result<types::AccountCreateResponse, AivenError> {
+		let url = "account";
+		let json_data = &[("account_name", account_name)];
+		let response = make_json_request!(self, reqwest::Method::POST, url, json_data)?;
+		Ok(response.json().await?)
+	}
 }
 
 #[cfg(test)]
 mod tests {
+	use crate::client::{encode_param, HTTPClient};
 	use crate::testutil;
 
 	#[tokio::test]
@@ -150,4 +293,72 @@ mod tests {
 			Err(e) => assert!(false, format!("{:?}", e)),
 		}
 	}
+
+	#[tokio::test]
+	async fn test_account_delete() {
+		let client = testutil::prepare_test_client();
+		let query_url = format!(
+			"/account/{account_id}/authentication/{account_authentication_method_id}",
+			account_id = encode_param("account_id"),
+			account_authentication_method_id = encode_param("someid")
+		);
+
+		let _m = testutil::create_mock_server(&query_url, "", "DELETE");
+
+		match client.account().delete_auth_method("account_id", "someid").await {
+			Ok(_) => assert!(true),
+			Err(e) => assert!(false, format!("{:?}", e)),
+		}
+	}
+
+	#[tokio::test]
+	async fn test_account_get_auth_method_details() {
+		let client = testutil::prepare_test_client();
+		let query_url = format!(
+			"/account/{account_id}/authentication/{account_authentication_method_id}",
+			account_id = encode_param("account_id"),
+			account_authentication_method_id = encode_param("someid")
+		);
+
+		let test_data = testutil::get_test_data("tests/testdata/account/get_auth_details.json");
+		let _m = testutil::create_mock_server(&query_url, &test_data, "GET");
+
+		match client.account().get_auth_method_details("account_id", "someid").await {
+			Ok(response) => assert!(response.authentication_method.account_id == "unique-account-id"),
+			Err(e) => assert!(false, format!("{:?}", e)),
+		}
+	}
+
+	#[tokio::test]
+	async fn test_account_update_auth_method() {
+		let client = testutil::prepare_test_client();
+		let query_url = format!(
+			"/account/{account_id}/authentication/{account_authentication_method_id}",
+			account_id = encode_param("account_id"),
+			account_authentication_method_id = encode_param("someid")
+		);
+
+		let test_data = testutil::get_test_data("tests/testdata/account/update_auth_method.json");
+		let _m = testutil::create_mock_server(&query_url, &test_data, "PUT");
+
+		match client.account().update_auth_method("account_id", "someid").await {
+			Ok(response) => assert!(response.authentication_method.account_id == "updated-account-id"),
+			Err(e) => assert!(false, format!("{:?}", e)),
+		}
+	}
+
+	#[tokio::test]
+	async fn test_account_create_new() {
+		let client = testutil::prepare_test_client();
+		let query_url = "/account";
+
+		let test_data = testutil::get_test_data("tests/testdata/account/create_new.json");
+		let _m = testutil::create_mock_server(&query_url, &test_data, "POST");
+
+		match client.account().create_new("account_name").await {
+			Ok(response) => assert!(response.account.account_name == "new-account-name"),
+			Err(e) => assert!(false, format!("{:?}", e)),
+		}
+	}
+
 }
