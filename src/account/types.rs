bd04770d6bb55966fbe0facbee4fd859f13b83e7
Ankur Srivastava <best.ankur@gmail.com>
2020-10-17 00:17:12 +0200

    more account implementations
    Signed-off-by: Ankur Srivastava <best.ankur@gmail.com>

--- src/account/types.rs
+++ src/account/types.rs
@@ -69,7 +69,7 @@ pub struct AuthenticationMethodsResponse {
 }
 
 #[derive(Deserialize, Serialize, Debug, Default)]
-pub struct AccountCreateResponse {
+pub struct AccountResponse {
 	pub account: Account,
 }
 
@@ -81,4 +81,89 @@ pub struct Account {
 	pub create_time: String,
 	pub is_account_owner: bool,
 	pub update_time: String,
+}
+
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Accounts {
+	pub accounts: Vec<Account>
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Event {
+
+	  pub account_id: String,
+      pub action_description: String,
+      pub action_type: String,
+      pub actor: String,
+      pub actor_user_id: String,
+      pub create_time: String,
+      pub log_entry_id: i64,
+      pub team_id: String
+}
+
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Events {
+	pub events: Vec<Event>
+}
+
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Email {
+	pub email: String
+}
+
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct CardInfo {
+	pub brand : String,
+	pub card_id : String,
+	pub country : String,
+	pub country_code : String,
+	pub exp_month : i32,
+	pub exp_year : i64,
+	pub last4 : String,
+	pub name : String,
+	pub user_email : String
+}
+
+      
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Project {
+	
+	  pub account_id: String,
+      pub account_name: String,
+      pub address_lines: serde_json::Value,
+      pub available_credits : String,
+      pub billing_address : String,
+      pub billing_currency : String,
+      pub billing_emails: Vec<Email>,
+      pub billing_extra_text:  String,
+      pub billing_group_id:  String,
+      pub billing_group_name:  String,
+      pub card_info: CardInfo,
+      pub city: String,
+      pub company: String,
+      pub country: String,
+      pub country_code: String,
+      pub default_cloud: String,
+      pub estimated_balance: String,
+      pub features: serde_json::Value,
+      pub payment_method: String,
+      pub project_name: String,
+      pub state: String,
+	  pub tech_emails: Vec<Email>,
+	  
+      pub tenant_id : String,
+      pub trial_expiration_time : String,
+      pub vat_id : String,
+      pub zip_code : String
+    }
+
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Projects {
+	pub projects: Vec<Project>,
+	pub total_project_count: i64,
 }
